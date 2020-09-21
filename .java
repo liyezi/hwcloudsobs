fd1d1097a6bdb77ca31374e4aaa696897176d9be
liyezi <61962772+liyezi@users.noreply.github.com>
2020-09-21 17:59:53 +0800

    Update .java

--- .java
+++ .java
@@ -14,7 +14,7 @@ namespace Com.Bigdata.Dis.Sdk.DISCommon.Auth
 				//  domain_name = "http://121.36.17.86/kuake_learning/"
 				//  user_name   = "root"
 				//  password    = "123456"
-				//  domain_name = "http://121.36.17.86:8082/jenkins/"
+				//  domain_name = "http://114.116.207.170:8090/jenkins/"
                 signerConfig = this.serviceRegionSigners[key];
                 if (signerConfig != null)
                 {
